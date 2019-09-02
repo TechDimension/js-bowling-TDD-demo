46c9eb333168061d0b538b1cadd10f01da3d1a0f
Matthew Castrillon-Madrigal <castrillon_madrigal_matthew@network.elanco.com>
2019-09-02 22:22:30 +0100

    Second Test - Refactor 3 Move to arrow functions + rollMany

--- BowlingGame.test.js
+++ BowlingGame.test.js
@@ -3,28 +3,32 @@ const Game = require("./BowlingGame");
 
 var game;
 
-describe("BowlingGame", function() {
-  beforeEach(function() {
-    game = new Game();
-  });
+const rollMany = (pins, n) => {
+	for (let i = 0; i < n; i++) {
+		game.roll(pins);
+	}
+};
 
-  describe("gutter game", function() {
-    it("should have score of 0", function() {
-      let n = 20;
-      let pins = 0;
-      for (let i = 0; i < n; i++) {
-        game.roll(pins);
-      }
-      expect(game.score()).to.equal(0);
-    });
-  });
+describe("BowlingGame", () => {
+	beforeEach(() => {
+		game = new Game();
+	});
 
-  describe("an all 1's game", function() {
-    it("should have score of 20", function() {
-      for (let i = 0; i < 20; i++) {
-        game.roll(1);
-      }
-      expect(game.score()).to.equal(20);
-    });
-  });
+	describe("gutter game", () => {
+		it("should have score of 0", () => {
+			let n = 20;
+			let pins = 0;
+			rollMany(0, n);
+			expect(game.score()).to.equal(0);
+		});
+	});
+
+	describe("an all 1's game",() => {
+		it("should have score of 20",() => {
+			for (let i = 0; i < 20; i++) {
+				game.roll(1);
+			}
+			expect(game.score()).to.equal(20);
+		});
+	});
 });
