ed54de5363baa2686b8b814a6e9a541b75d9de0d
Matthew Castrillon-Madrigal <castrillon_madrigal_matthew@network.elanco.com>
2019-09-02 22:51:51 +0100

    Third Test - Refactor 1 Had to refactor. Better names, functions do what they are named

--- BowlingGame.js
+++ BowlingGame.js
@@ -1,12 +1,17 @@
 class Game {
   constructor() {
-    this.s = 0;
+	this.rolls = new Array(21);
+    this.currentRoll = 0;
   }
   roll(pins) {
-    this.s += pins;
+    this.rolls[this.currentRoll++] = pins;
   }
   score() {
-    return this.s;
+	let score = 0;
+	for (let i=0; i< this.rolls.length - 1 ; i ++){
+	  score += this.rolls[i];
+	}
+	return score;
   }
 }
 
