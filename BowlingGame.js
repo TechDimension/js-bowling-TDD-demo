76212db56239456d14c2e365b57dfae363f020c5
Matthew Castrillon-Madrigal <castrillon_madrigal_matthew@network.elanco.com>
2019-09-02 23:10:14 +0100

    Third Test - Green 2 Calculates a spare in a frame

--- BowlingGame.js
+++ BowlingGame.js
@@ -1,19 +1,24 @@
 class Game {
   constructor() {
-	this.rolls = new Array(21);
+    this.rolls = new Array(21);
     this.currentRoll = 0;
   }
   roll(pins) {
     this.rolls[this.currentRoll++] = pins;
   }
   score() {
-	let score = 0;
-	let i = 0;
-	  for (let frame=0; frame < 10 ; frame ++){
-	  score += this.rolls[i] + this.rolls[i+1];
-	  i += 2;
-	}
-	return score;
+    let score = 0;
+    let i = 0;
+    for (let frame = 0; frame < 10; frame++) {
+      if (this.rolls[i] + this.rolls[i + 1] === 10) {
+        score += 10 + this.rolls[i + 2];
+        i += 2;
+      } else {
+        score += this.rolls[i] + this.rolls[i + 1];
+        i += 2;
+      }
+    }
+    return score;
   }
 }
 
