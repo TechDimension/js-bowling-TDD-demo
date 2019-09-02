cb8c99244d89881cfde3de39c48e8f7ea4d3640d
Matthew Castrillon-Madrigal <castrillon_madrigal_matthew@network.elanco.com>
2019-09-02 21:21:00 +0100

    First Test - Red 3 Score should be 0 with a gutter game

--- BowlingGame.test.js
+++ BowlingGame.test.js
@@ -1,13 +1,14 @@
-const expect = require("chai");
+const { expect } = require("chai");
 const Game = require("./BowlingGame");
 
 describe("BowlingGame", function() {
   describe("gutter game", function() {
-    it("Runs", function() {
+    it("should have score of 0", function() {
       const game = new Game();
       for (let i = 0; i < 20; i++) {
         game.roll(0);
       }
+      expect(game.score).to.equal(0);
     });
   });
 });
