90934899857571631930b456141e527e8e7b54b3
Matthew Castrillon-Madrigal <castrillon_madrigal_matthew@network.elanco.com>
2019-09-02 18:47:54 +0100

    First Test - Green 2 Going back to test as need to require

--- BowlingGame.test.js
+++ BowlingGame.test.js
@@ -1,8 +1,9 @@
 const expect = require("chai");
+const Game = require("./BowlingGame");
 
 describe("BowlingGame", function() {
   describe("gutter game", function() {
-    it("runs", function() {
+    it("Runs", function() {
       const game = new Game();
     });
   });
