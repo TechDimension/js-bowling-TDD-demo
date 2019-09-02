44ff2c25756714abf05bf3d5edb3061106f53e3b
Matthew Castrillon-Madrigal <castrillon_madrigal_matthew@network.elanco.com>
2019-09-02 21:43:29 +0100

    Second Test - Refactor 1 Duplication of new Game

--- BowlingGame.test.js
+++ BowlingGame.test.js
@@ -1,10 +1,15 @@
 const { expect } = require("chai");
 const Game = require("./BowlingGame");
 
+var game;
+
 describe("BowlingGame", function() {
+  beforeEach(function() {
+    game = new Game();
+  });
+
   describe("gutter game", function() {
     it("should have score of 0", function() {
-      const game = new Game();
       for (let i = 0; i < 20; i++) {
         game.roll(0);
       }
@@ -14,7 +19,6 @@ describe("BowlingGame", function() {
 
   describe("an all 1's game", function() {
     it("should have score of 20", function() {
-      const game = new Game();
       for (let i = 0; i < 20; i++) {
         game.roll(1);
       }
