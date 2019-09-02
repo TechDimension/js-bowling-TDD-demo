64257fca355d6a4145ccef118ca07365f22b1383
Matthew Castrillon-Madrigal <castrillon_madrigal_matthew@network.elanco.com>
2019-09-02 18:37:38 +0100

    First Test: Red 1

--- BowlingGame.test.js
+++ BowlingGame.test.js
@@ -1,2 +1,9 @@
 const expect = require("chai");
-describe("BowlingGame", function() {});
+
+describe("BowlingGame", function() {
+  describe("gutter game", function() {
+    it("runs", function() {
+      const game = new Game();
+    });
+  });
+});
