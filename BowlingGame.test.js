7e4e5d5409f1725c0ac5db328ff0551159536e3a
Matthew Castrillon-Madrigal <castrillon_madrigal_matthew@network.elanco.com>
2019-09-02 21:30:54 +0100

    Second Test - Red 1 An all 1's Game should have a score of 20. Current implementation does not give anything other than 0

--- BowlingGame.test.js
+++ BowlingGame.test.js
@@ -11,4 +11,14 @@ describe("BowlingGame", function() {
       expect(game.score()).to.equal(0);
     });
   });
+
+  describe("an all 1's game", function() {
+    it("should have score of 20", function() {
+      const game = new Game();
+      for (let i = 0; i < 20; i++) {
+        game.roll(1);
+      }
+      expect(game.score()).to.equal(20);
+    });
+  });
 });
