a231fd821236f782db843653ef747be5dc4768ac
Matthew Castrillon-Madrigal <castrillon_madrigal_matthew@network.elanco.com>
2019-09-02 23:41:23 +0100

    Fourth Test - Refactor 4 isStrike() to use instead of inconditional logic

--- BowlingGame.js
+++ BowlingGame.js
@@ -10,7 +10,7 @@ class Game {
     let score = 0;
     let frameIndex = 0;
     for (let frame = 0; frame < 10; frame++) {
-      if (this.rolls[frameIndex] === 10) {
+      if (this.isStrike(frameIndex)) {
         score += 10 + this.strikeBonus(frameIndex);
         frameIndex++;
       } else if (this.isSpare(frameIndex)) {
@@ -26,6 +26,9 @@ class Game {
 
   isSpare(frameIndex) {
     return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
+  }
+  isStrike(frameIndex) {
+    return this.rolls[frameIndex] === 10;
   }
   strikeBonus(frameIndex) {
     return this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
